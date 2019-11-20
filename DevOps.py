7e607022c54dcca50d9b9848fe579a3e21f78c04
pengfeichen <chen19871120>
2019-11-20 16:14:24 +0800

    Add test code

---
+++ DevOps.py
@@ -0,0 +1,18 @@
+"""
+This is a sample code.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+@app.route('/test')
+def test():
+    return "This is a DevOps test"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8888)
